7408f059dccac34cb545a3845920ed0cce091659
caMelBG <shmekera1564@gmail.com>
2017-04-30 21:56:27 +0300

    Configurate CRUD operations

--- ContosoUniversity/ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/ContosoUniversity/Models/Student.cs
@@ -14,5 +14,7 @@ namespace ContosoUniversity.Models
         public DateTime EnrollmentDate { get; set; }
 
         public ICollection<Enrollment> Enrollments { get; set; }
+        
+        public string Secret { get; set; }
     }
 }
