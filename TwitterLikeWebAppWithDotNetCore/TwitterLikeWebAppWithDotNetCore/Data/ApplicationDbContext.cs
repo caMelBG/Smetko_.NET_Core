03c4bd18ab274e415b571b4ab389de0dc7bc51b5
caMelBG <shmekera1564@gmail.com>
2017-05-09 18:31:13 +0300

    DbContext updated

--- TwitterLikeWebAppWithDotNetCore/TwitterLikeWebAppWithDotNetCore/Data/ApplicationDbContext.cs
+++ TwitterLikeWebAppWithDotNetCore/TwitterLikeWebAppWithDotNetCore/Data/ApplicationDbContext.cs
@@ -15,12 +15,18 @@ namespace TwitterLikeWebAppWithDotNetCore.Data
         {
         }
 
+        public DbSet<Tweet> Tweets { get; set; }
+
+        public DbSet<Message> Messages { get; set; }
+
+        public DbSet<Image> Images { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            builder.Entity<Tweet>().ToTable("Tweet");
+            builder.Entity<Message>().ToTable("Message");
+            builder.Entity<Image>().ToTable("Image");
             base.OnModelCreating(builder);
-            // Customize the ASP.NET Identity model and override the defaults if needed.
-            // For example, you can rename the ASP.NET Identity table names and more.
-            // Add your customizations after calling base.OnModelCreating(builder);
         }
     }
 }
